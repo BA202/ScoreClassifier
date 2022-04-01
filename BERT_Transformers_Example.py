7fd9835b96d475dcc0c52c159d8ef354fd82adf5
TobiasRothlin <tobias@rothlin.com>
2022-04-01 09:05:07 +0200

    Uses hotel trainings data

--- BERT_Transformers_Example.py
+++ BERT_Transformers_Example.py
@@ -1,130 +1,27 @@
 from transformers import BertTokenizer, TFBertForSequenceClassification
 from transformers import InputExample, InputFeatures
-
-import os, ssl
-if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
-getattr(ssl, '_create_unverified_context', None)):
-    ssl._create_default_https_context = ssl._create_unverified_context
-
-model = TFBertForSequenceClassification.from_pretrained("bert-base-uncased")
-tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-
-model.summary()
-
 import tensorflow as tf
 import pandas as pd
-
-URL = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
-
-dataset = tf.keras.utils.get_file(fname="aclImdb_v1.tar.gz",
-                                  origin=URL,
-                                  untar=True,
-                                  cache_dir='.',
-                                  cache_subdir='')
-
-# The shutil module offers a number of high-level
-# operations on files and collections of files.
-import os
-import shutil
-# Create main directory path ("/aclImdb")
-main_dir = os.path.join(os.path.dirname(dataset), 'aclImdb')
-# Create sub directory path ("/aclImdb/train")
-train_dir = os.path.join(main_dir, 'train')
-# Remove unsup folder since this is a supervised learning task
-remove_dir = os.path.join(train_dir, 'unsup')
-shutil.rmtree(remove_dir)
-# View the final train folder
-print(os.listdir(train_dir))
-
-# We create a training dataset and a validation
-# dataset from our "aclImdb/train" directory with a 80/20 split.
-train = tf.keras.preprocessing.text_dataset_from_directory(
-    'aclImdb/train', batch_size=30000, validation_split=0.2,
-    subset='training', seed=123)
-test = tf.keras.preprocessing.text_dataset_from_directory(
-    'aclImdb/train', batch_size=30000, validation_split=0.2,
-    subset='validation', seed=123)
-
-for i in train.take(1):
-  train_feat = i[0].numpy()
-  train_lab = i[1].numpy()
-
-train = pd.DataFrame([train_feat, train_lab]).T
-train.columns = ['DATA_COLUMN', 'LABEL_COLUMN']
-train['DATA_COLUMN'] = train['DATA_COLUMN'].str.decode("utf-8")
-train.head()
-
-for j in test.take(1):
-  test_feat = j[0].numpy()
-  test_lab = j[1].numpy()
-
-test = pd.DataFrame([test_feat, test_lab]).T
-test.columns = ['DATA_COLUMN', 'LABEL_COLUMN']
-test['DATA_COLUMN'] = test['DATA_COLUMN'].str.decode("utf-8")
-test.head()
-
-InputExample(guid=None,
-             text_a = "Hello, world",
-             text_b = None,
-             label = 1)
+from DataHandler.DataHandler import DataHandler
 
 
 def convert_data_to_examples(train, test, DATA_COLUMN, LABEL_COLUMN):
-    train_InputExamples = train.apply(lambda x: InputExample(guid=None,
-                                                             # Globally unique ID for bookkeeping, unused in this case
-                                                             text_a=x[
-                                                                 DATA_COLUMN],
-                                                             text_b=None,
-                                                             label=x[
-                                                                 LABEL_COLUMN]),
-                                      axis=1)
-
-    validation_InputExamples = test.apply(lambda x: InputExample(guid=None,
-                                                                 # Globally unique ID for bookkeeping, unused in this case
-                                                                 text_a=x[
-                                                                     DATA_COLUMN],
-                                                                 text_b=None,
-                                                                 label=x[
-                                                                     LABEL_COLUMN]),
-                                          axis=1)
+    train_InputExamples = train.apply(lambda x: InputExample(guid=None,text_a=x[DATA_COLUMN],text_b=None,label=x[LABEL_COLUMN]),axis=1)
+    validation_InputExamples = test.apply(lambda x: InputExample(guid=None,text_a=x[DATA_COLUMN],text_b=None,label=x[LABEL_COLUMN]),axis=1)
 
     return train_InputExamples, validation_InputExamples
-
-    train_InputExamples, validation_InputExamples = convert_data_to_examples(
-        train,
-        test,
-        'DATA_COLUMN',
-        'LABEL_COLUMN')
-
 
 def convert_examples_to_tf_dataset(examples, tokenizer, max_length=128):
     features = []  # -> will hold InputFeatures to be converted later
 
     for e in examples:
+
         # Documentation is really strong for this method, so please take a look at it
-        input_dict = tokenizer.encode_plus(
-            e.text_a,
-            add_special_tokens=True,
-            max_length=max_length,  # truncates if len(s) > max_length
-            return_token_type_ids=True,
-            return_attention_mask=True,
-            pad_to_max_length=True,
-            # pads to the right by default # CHECK THIS for pad_to_max_length
-            truncation=True
-        )
+        input_dict = tokenizer.encode_plus(e.text_a,add_special_tokens=True,max_length=max_length,return_token_type_ids=True,return_attention_mask=True,pad_to_max_length=True, truncation=True)
 
-        input_ids, token_type_ids, attention_mask = (input_dict["input_ids"],
-                                                     input_dict[
-                                                         "token_type_ids"],
-                                                     input_dict[
-                                                         'attention_mask'])
+        input_ids, token_type_ids, attention_mask = (input_dict["input_ids"],input_dict["token_type_ids"],input_dict['attention_mask'])
 
-        features.append(
-            InputFeatures(
-                input_ids=input_ids, attention_mask=attention_mask,
-                token_type_ids=token_type_ids, label=e.label
-            )
-        )
+        features.append(InputFeatures(input_ids=input_ids, attention_mask=attention_mask,token_type_ids=token_type_ids, label=e.label))
 
     def gen():
         for f in features:
@@ -152,6 +49,20 @@ def convert_examples_to_tf_dataset(examples, tokenizer, max_length=128):
     )
 
 
+
+model = TFBertForSequenceClassification.from_pretrained("bert-base-uncased")
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+
+model.summary()
+
+myDataHandler = DataHandler("/Users/tobiasrothlin/Documents/BachelorArbeit/DataSets/ClassifiedDataSetV1.3")
+
+data = myDataHandler.getScoreData()
+data = [frame for frame in data if not frame[1] == "Neutral"]
+
+train = pd.DataFrame(data[:-100],columns=['DATA_COLUMN', 'LABEL_COLUMN'])
+test = pd.DataFrame(data[-100:-10],columns = ['DATA_COLUMN', 'LABEL_COLUMN'])
+
 DATA_COLUMN = 'DATA_COLUMN'
 LABEL_COLUMN = 'LABEL_COLUMN'
 
@@ -169,8 +80,8 @@ model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=3e-5, epsilon=1e-
 
 model.fit(train_data, epochs=2, validation_data=validation_data,)
 
-pred_sentences = ['This was an awesome movie. I watch it twice my time watching this beautiful movie if I have known it was this good',
-                  'One of the worst movies of all time. I cannot believe I wasted two hours of my life for this movie']
+pred_sentences = [frame[0] for frame in data[-10:]]
+Lbl_pred_sentences = [frame[1] for frame in data[-10:]]
 
 
 tf_batch = tokenizer(pred_sentences, max_length=128, padding=True, truncation=True, return_tensors='tf')
@@ -180,4 +91,4 @@ labels = ['Negative','Positive']
 label = tf.argmax(tf_predictions, axis=1)
 label = label.numpy()
 for i in range(len(pred_sentences)):
-  print(pred_sentences[i], ": \n", labels[label[i]])
+  print(pred_sentences[i], ": \n", labels[label[i]],Lbl_pred_sentences[i])
