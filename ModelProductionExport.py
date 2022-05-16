34a3e541396e22496b967a49b6b3e50bc6fb5b40
TobiasRothlin <tobias@rothlin.com>
2022-05-16 16:54:18 +0200

    Implemented multilable classification

--- ModelProductionExport.py
+++ ModelProductionExport.py
@@ -1,4 +1,4 @@
-from transformers import TFBertModel, TFAutoModelForSequenceClassification
+from transformers import TFBertModel, TFAutoModelForSequenceClassification, TFAutoModelForMultipleChoice
 from transformers import AutoTokenizer
 import transformers
 import os
@@ -9,6 +9,7 @@ from DataHandler.DataHandler import DataHandler
 import numpy as np
 import random
 import json
+from tensorflow.keras.utils import to_categorical
 
 os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = "true"
 
@@ -18,7 +19,7 @@ epochs = 5
 learning_rate = 5e-5
 seed = 6.838324
 language = "English"
-path = f"{modelName}_{language}_sentiment"
+path = f"{modelName}_{language}_classification"
 
 productionReport = ModelReport(
     modelName,
@@ -34,32 +35,41 @@ productionReport = ModelReport(
 
 
 dataHandler = DataHandler("", language)
-trainingData = dataHandler.getScoreData()
+trainingData = dataHandler.getCategorieData("Location",multilablel=True)
+temp = {}
+for sample in trainingData:
+    if sample[0] in temp.keys():
+        temp[sample[0]].add(sample[1])
+    else:
+        temp[sample[0]] = {sample[1]}
 
-random.seed(seed)
-random.shuffle(trainingData)
+trainingData = [[key,list(temp[key]) ]for key in temp.keys()]
+for sample in trainingData:
+    pass
+    #print(sample[0],sample[1])
+#random.seed(seed)
+#random.shuffle(trainingData)
 
 testData = trainingData[-100:]
 trainingData = trainingData[:-100]
 
-setOfCats = list({sample[1] for sample in trainingData})
+setOfCats = list({sample[1][0] for sample in trainingData})
 catToInt = {cat: i for i, cat in enumerate(list(setOfCats))}
 intToCat = {catToInt[key]: key for key in catToInt.keys()}
 
 tokenizer = AutoTokenizer.from_pretrained(modelName)
 model = TFAutoModelForSequenceClassification.from_pretrained(
-    modelName, from_pt=True, num_labels=len(setOfCats)
+    modelName, from_pt=True, num_labels=len(setOfCats), problem_type="multi_label_classification"
 )
-
 tfTrainingData = []
 
 productionReport.addTrainingSet(trainingData)
 
 optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
-loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
 metric = tf.metrics.SparseCategoricalAccuracy()
-model.compile(optimizer=optimizer, loss=loss, metrics=metric)
-
+model.compile(optimizer=optimizer, loss="binary_crossentropy", metrics=["categorical_accuracy"])
+model.summary()
 tokSens = tokenizer(
     [sample[0] for sample in trainingData],
     add_special_tokens=True,
@@ -71,11 +81,15 @@ tokSens = tokenizer(
     return_attention_mask=True,
     verbose=True,
 )
-
+temp = []
+for sample in trainingData:
+    #print(sample)
+    temp.append(sum(np.array([to_categorical(catToInt[lbl],num_classes=len(setOfCats)) for lbl in sample[1]])))
+y = np.array(temp)
 
 model.fit(
-    x=tokSens["input_ids"],
-    y=np.array([catToInt[sample[1]] for sample in trainingData]),
+    x=tokSens['input_ids'],
+    y=y,
     validation_split=0,
     batch_size=64,
     epochs=epochs,
@@ -83,6 +97,7 @@ model.fit(
 
 
 def classify(sentence):
+    return intToCat[0]
     senVec = tokenizer(
         text=[sentence],
         add_special_tokens=True,
--- PipelineInterface.py
+++ PipelineInterface.py
@@ -7,8 +7,28 @@ class PipelineInterface:
 
     def classify(self,sentence):
         res = self.__pipe(sentence)
+        print(res)
         return res[0]['label'],res[0]['score']
 
 if __name__ == '__main__':
-    myClass = PipelineInterface("Tobias/bert-base-uncased_English_Hotel_sentiment")
-    print(myClass.classify("The hotel is very nicely located"))
+    modelName = "bert-base-uncased_English_sentiment"
+    myClass = PipelineInterface(modelName)
+    from DataHandler.DataHandler import DataHandler
+    myDataHander = DataHandler()
+    myScoreData = myDataHander.getScoreData()
+    myScoreData = [sample for sample in myScoreData if not sample[1] == "Neutral"]
+    i = 0
+    correct = 0
+    with open(modelName + "_WrongClassification.tsv", 'w') as outputFile:
+        outputFile.write(
+            "Sentence" + "\t" + "Prediction" + "\t" + "True" + "\n")
+    for sample in myScoreData:
+        print(i)
+        i += 1
+        predicted = myClass.classify(sample[0])
+        if predicted[0] == sample[1]:
+            correct += 1
+        else:
+            with open(modelName+"_WrongClassification.tsv",'a') as outputFile:
+                outputFile.write(sample[0] + "\t" + predicted[0] + "\t" + sample[1] + "\n")
+    print(f"Accuracy: {correct/i}")
